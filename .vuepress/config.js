8ebd20af173cd50e986d94acf61f0e080ef61306
Lucas Henrique <lhs_azevedo@hotmail.com>
2020-10-16 02:34:29 -0300

    Extract head config to separate file

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -3,52 +3,7 @@ module.exports = {
     description: "Beautifully designed application scaffolding for Laravel",
     base: '/',
 
-    head: [
-        [
-            'link',
-            {
-                href:
-                    'https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,800,800i,900,900i',
-                rel: 'stylesheet',
-                type: 'text/css',
-            },
-        ], [
-            'link',
-            {
-                rel: 'apple-touch-icon',
-                sizes: '180x180',
-                href: '/assets/img/apple-touch-icon.png?v=00aYpBgAp5'
-            }
-        ], [
-            'link',
-            {
-                rel: 'icon',
-                type: 'image/png',
-                sizes: '32x32',
-                href: '/assets/img/favicon-32x32.png?v=00aYpBgAp5'
-            }
-        ], [
-            'link',
-            {
-                rel: 'icon',
-                type: 'image/png',
-                sizes: '16x16',
-                href: '/assets/img/favicon-16x16.png?v=00aYpBgAp5'
-            }
-        ], [
-            'link',
-            {
-                rel: 'shortcut icon',
-                href: '/assets/img/favicon.ico?v=00aYpBgAp5'
-            }
-        ], [
-            'meta',
-            {
-                name: 'theme-color',
-                content: '#ffffff'
-            }
-        ],
-    ],
+    head: require('./head'),
 
     themeConfig: {
         logo: '/assets/img/logo.svg',
---
+++ .vuepress/head.js
@@ -0,0 +1,46 @@
+module.exports = [
+    [
+        'link',
+        {
+            href:
+                'https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,800,800i,900,900i',
+            rel: 'stylesheet',
+            type: 'text/css',
+        },
+    ], [
+        'link',
+        {
+            rel: 'apple-touch-icon',
+            sizes: '180x180',
+            href: '/assets/img/apple-touch-icon.png?v=00aYpBgAp5'
+        }
+    ], [
+        'link',
+        {
+            rel: 'icon',
+            type: 'image/png',
+            sizes: '32x32',
+            href: '/assets/img/favicon-32x32.png?v=00aYpBgAp5'
+        }
+    ], [
+        'link',
+        {
+            rel: 'icon',
+            type: 'image/png',
+            sizes: '16x16',
+            href: '/assets/img/favicon-16x16.png?v=00aYpBgAp5'
+        }
+    ], [
+        'link',
+        {
+            rel: 'shortcut icon',
+            href: '/assets/img/favicon.ico?v=00aYpBgAp5'
+        }
+    ], [
+        'meta',
+        {
+            name: 'theme-color',
+            content: '#ffffff'
+        }
+    ],
+]
